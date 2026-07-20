4bfbe607a3746656974d659ae3e9a9fb80bf6b44
aungwin <babybird1801@gmail.com>
2026-07-21 00:19:22 +0630

    adding restaurant file in main page

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,21 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "pages#home"
   get '/menu', to: 'menu#index', as: :menu
-  get '/reservation', to: 'reservations#new', as: :new_reservation
-  post '/reservation', to: 'reservations#create', as: :reservations
+  # get '/reservation', to: 'reservations#new', as: :new_reservation
+  # post '/reservation', to: 'reservations#create', as: :reservations
   get '/contact', to: 'pages#contact', as: :contact
   
-  resources :restaurants do
-    resources :reservations
-    resources :orders
-    resources :tables
-    resources :menu
+  resources :restaurants, only: [:index, :show] do
+    resources :users, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+    resources :menu, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+    resources :tables, only: [:index, :show, :new, :create, :edit, :update]
+    resources :reservations, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+    resources :orders, only: [:index, :show, :new, :create, :update] do
+      collection do
+        get :export
+      end
+      # resource :payment, only: [:create]
+      # post :refund, to: 'payments#refund'
+    end
   end
 end
